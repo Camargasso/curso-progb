799c6f0320bc2eeeffe2cf0fa7ded3f2c8a91eef
Gabriel Camargo Alves <140025786+Camargasso@users.noreply.github.com>
2023-08-31 17:16:28 -0300

    Add files via upload

---
+++ Curso-progbr/Java/script.js
@@ -0,0 +1,36 @@
+// // alert("Hello word")
+// console.log("Olá Mundo");
+a="Placa de captura";
+b="erro";
+var b = a;
+console.log(b);
+
+
+// String é uma cadeia de caracteres (palavras ou frases) e estão sempre dentro de aspas
+var b = "Primeira linha \n Segunda linha "
+console.log(b);
+
+console.log("1" + "1");
+console.log( 1 + 1);
+
+var b = null;
+console.log(b);
+
+// Boolean vai ser true / False
+
+a = 10                   
+b = 15
+c = a * b
+console.log(c)
+// % módulo é a sobra do resultado da divisão 17/15=1 e sobra 2 logo 2 sera o módulo
+// ++ incremento 
+// -- decremento 
+a = 20
+b = --a    /*poderiamos usar incremento e decremento*/ 
+console.log(b)
+
+
+// a= a + 5 mesma coisa que a variável abaixo 
+a = 30
+a +=5
+console.log(a)
