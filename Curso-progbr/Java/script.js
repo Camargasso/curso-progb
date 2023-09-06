9567f77a72cc50520bae2d052bba888d8179aadd
Gabriel Camargo Alves <140025786+Camargasso@users.noreply.github.com>
2023-09-06 17:41:48 -0300

    Add files via upload

--- Curso-progbr/Java/script.js
+++ Curso-progbr/Java/script.js
@@ -68,7 +68,7 @@ console.log(entre)
 var idade = 8
 var menor10 = idade <= 10
 var maior65 = idade >= 65
-var gratuidade = menor10 || maior65   /* || = ou */ 
+var gratuidade = menor10 || maior65   /* || = ou */
 console.log(gratuidade)
 
 var idade = 35
@@ -90,37 +90,102 @@ console.log(c)
 var nome = prompt('Insira seu nome:')
 alert('Bom dia, ' + nome)
 
-const larguraTela = window.innerWidth;
-const alturaTela = window.innerHeight;
-console.log(`Resolução da tela: ${larguraTela} x ${alturaTela}`);
+// const larguraTela = window.innerWidth;
+// const alturaTela = window.innerHeight;
+// console.log(`Resolução da tela: ${larguraTela} x ${alturaTela}`);
 
-alert(`Resolução da tela: ${larguraTela} x ${alturaTela}`)
+// alert(`Resolução da tela: ${larguraTela} x ${alturaTela}`)
 
 var idade = 22
 
-if (idade >= 35){
+if (idade >= 35) {
     console.log('pode')
     console.log('Qual seu pedido')
 }
 
-else if (idade >= 18){
+else if (idade >= 18) {
     console.log('pode')
     console.log('Mostre a identidade')
 }
 
-else{
+else {
     console.log('não pode')
     console.log('Volte futuramente')
 }
 
 var idade = 22
 
-if (idade >= 18 && idade <= 70){
+if (idade >= 18 && idade <= 70) {
     console.log('pode')
     console.log('Qual seu pedido')
 }
 
-else{
+else {
     console.log('não pode')
     console.log('Volte futuramente')
 }
+
+var idade = 17
+idade >= 18 ? console.log('Pode') : console.log('Não pode')
+
+
+// -----------------------------------------Outro exercício------------------------------------
+
+
+var nota1 = 5.0
+var nota2 = 5.8
+
+var media = (nota1 + nota2) / 2
+
+var conceito = ''
+if (media >= 8) {
+    conceito = 'Ótimo'
+}
+else if (media >= 6.5) {
+    conceito = 'Bom'
+}
+else {
+    conceito = 'Regular'
+}
+console.log(media)
+console.log(conceito)
+
+switch (conceito) {
+    case 'Ótimo':
+        console.log('Parabéns')
+        break;
+    case 'Bom':
+        console.log('Você esta quase perfeito')
+        break;
+    case 'Regular': 
+        console.log('Estude mais um pouco')
+        break;
+    default:
+        console.log('Houve algum erro')
+        break;
+}
+
+
+// for serve para repitir algo um certo número de vezes
+var numero = 5
+for (var i = 0; i < numero; i++) {
+    console.log('Executando o for, pela  ' + i + 'Vez')
+}
+console.log('Acabou')   
+
+
+var numero = 5
+while(numero < 10){
+console.log('Número' + numero)
+numero++
+}
+console.log('Acabou')
+
+
+var numero = Math.random() * 100
+console.log(numero)
+while(numero < 90){
+    var numero = Math.random() * 100
+}
+console.log(numero)
+console.log('Acabou')
