b4c5690f93d40a2878d0a3d4daf72bc4e78fb5eb
Gabriel Camargo Alves <140025786+Camargasso@users.noreply.github.com>
2023-10-06 16:57:31 -0300

    Add files via upload

--- Curso-progbr/Html avançado/script.js
+++ Curso-progbr/Html avançado/script.js
@@ -15,9 +15,15 @@ console.log(id)
 let num = parseInt(lista.dataset.n) //parseInt transforma a string em um numero
 console.log(lista.dataset.num)
 
-let conteudo = ""
+let conteudo = " "
 for(let i = 0; i< num; i++){
 conteudo += "<li>" + i + "</li>"
 }
 
 lista.innerHTML = conteudo
+
+//vídeo pt2.----------------------------------------------------------------------------------------
+let video = document.getElementById("video1")
+function play(){
+    video.play()
+}
