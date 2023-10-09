ebbbb50b6498c0cea0d28df776d8c4ead8e7af08
Gabriel Camargo Alves <140025786+Camargasso@users.noreply.github.com>
2023-10-09 18:26:35 -0300

    Add files via upload

--- Curso-progbr/Html avançado/script.js
+++ Curso-progbr/Html avançado/script.js
@@ -27,3 +27,22 @@ let video = document.getElementById("video1")
 function play(){
     video.play()
 }
+
+//Animação com HTMl-----------------------------------------------------------------------------------
+let jogador = document.getElementById("jogador")
+
+let xInicial = 0
+let yInicial = 0
+
+function moverJogadorPara(x,y){
+let posX = x + "px"
+let posY = y + "px"
+
+jogador.style.top = posX
+jogador.style.left = posY
+
+}
+
+setInterval (function(){
+// moverJogadorPara(xInicial++, yInicial++) descomentar para ativar essa função(teste com animação)
+}, 27)
