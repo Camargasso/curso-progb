35407f4dcc42ede5fc945a1cfed115a2f9040534
Gabriel Camargo Alves <140025786+Camargasso@users.noreply.github.com>
2023-11-10 17:15:19 -0300

    Add files via upload

--- Curso-progbr/Js avançado/script.js
+++ Curso-progbr/Js avançado/script.js
@@ -30,3 +30,21 @@ let dobro = x => 2 * x
 console.log(dobro(5))
 
 //fazendo a mesma coisa utilzando a função arrow (arrow não trabalha bem com o this)
+
+
+//----------------------------------------------Callback--------------------------------------------------
+let usuarios = ["Adriano", "Marcia", "José"]
+
+function inserirUsuario(nome, callback) {
+    setTimeout(() =>{
+        usuarios.push(nome)
+        callback()                   //callback aplica o delay automaticamente para executar a função
+    },1000)
+
+}
+
+function listarUsuarios(){
+    console.log(usuarios)
+}
+
+inserirUsuario("Igor", listarUsuarios)
